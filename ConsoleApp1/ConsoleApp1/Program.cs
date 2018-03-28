797eda761b1cce1277022851c73011e45b43069f
plisewski <32065830+plisewski@users.noreply.github.com>
2018-03-28 18:38:49 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("testing gihub in VS");
+            Console.WriteLine("testing gihub in VS #1");
         }
     }
 }
