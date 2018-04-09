f141a3b7e288c22015d898d97bdb995f720aec13
Piotr Lisewski <p.lisewski@hotmail.com>
2018-04-09 22:22:17 +0200

    local refactoring

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
             Console.WriteLine("testing gihub in VS #1");
             Console.WriteLine("testing gihub in VS #2");
             Console.WriteLine("testing gihub in VS #n");
